e1b0fe1d3273dd802d36392949ccb7b16aef6119
Vasyl Khomenko <vkhomenko@qubell.com>
2016-02-10 13:43:53 +0200

    Make monitor be accessible as script

--- setup.py
+++ setup.py
@@ -17,17 +17,19 @@ test_requires = [
     ]
 
 setup(name='qubell-api-python-client',
-      version='1.43.44.12',  # versionising: <major>.<minor>.<platform major>.<platform minor>
+      version='1.44.44.12',  # versionising: <major>.<minor>.<platform major>.<platform minor>
       description='Qubell platform client library',
       long_description=open('README').read(),
       author='Vasyl Khomenko',
       author_email='vkhomenko@qubell.com',
       license=open('LICENSE').read(),
       url='https://github.com/qubell/contrib-python-qubell-client',
-      packages=find_packages(exclude=['test_qubell_client', 'stories']),
-      package_data={'': ['LICENSE', 'README']},
+      packages=find_packages(exclude=['test_qubell_client', 'stories', 'integration_tests', 'integration_tests.testing']),
+      package_data={'': ['LICENSE', 'README', 'monitor/monitor_manifests/*']},
       include_package_data=True,
       install_requires=install_requires,
       tests_require=test_requires,
       test_suite="nosetests",
+      entry_points={
+        'console_scripts': 'qubell_monitor = qubell.monitor.monitor:main'},
      )
